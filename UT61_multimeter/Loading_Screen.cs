2a6410ea94e187ab04d39569546a26fd3fa26c84
Ptr <krolak.piotr.lca@gmail.com>
2019-10-27 18:53:14 +0100

    ~Dodanie projektu

---
+++ UT61_multimeter/Loading_Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UT61_multimeter
+{
+    public partial class Loading_Screen : Form
+    {
+        public Loading_Screen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UT61_multimeter/Main_Form.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace UT61_multimeter
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.SuspendLayout();
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main_Form";
+            this.Text = "UT61C";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
